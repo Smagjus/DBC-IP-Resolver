347b334309ac0b63548e13fadf2f0a19c644b7c8
Smagjus <Smagjus@users.noreply.github.com>
2015-02-24 16:05:59 +0100

    Admin requirement

--- IP Resolver/Program.cs
+++ IP Resolver/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Security.Principal;
+using System.Diagnostics;
+
 
 namespace IP_Resolver
 {
@@ -13,6 +16,25 @@ namespace IP_Resolver
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool administrativeMode = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!administrativeMode)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.Verb = "runas";
+                startInfo.FileName = Application.ExecutablePath;
+                try
+                {
+                    Process.Start(startInfo);
+                }
+                catch
+                {
+                    return;
+                }
+                return;
+            }
             Application.Run(new Form1());
         }
     }
